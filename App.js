74acb506bfaa35a84cd181cdc042c4e75af085e8
SACHIN RANA <sachinrana@SACHINs-MacBook-Air.local>
2023-03-17 12:31:31 +0530

    completed the first lec

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [name, setName ] = useState('Ram');
+  const [session, setSession] = useState({number:6,title: 'state'});
+  const [current, setCurrent] = useState(true);
+  const onClickHandler = () => {
+    if(name == 'Ram')
+      setName('Jai Shree ram');
+    else {
+      setName('Ram');
+    }
+    setSession({number : session.number+1, title: 'style'});
+    setCurrent(false);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>This {session.title} and number is {session.number} </Text>
+      <Text style={styles.text}>{current ? 'current-session' : 'next-session'}</Text>
+      <View style={styles.button}>
+        <Button title='Update State' onPress={onClickHandler}></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f0f',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#493fff',
+    borderRadius: 10,
   },
+  text: {
+    color: '#ffffff',
+    fontSize: 25,
+    fontStyle: 'italic',
+    margin: 10,
+    textTransform: 'uppercase',
+  },
+  button: {
+    width: 200,
+    height: 60,
+  }
 });
