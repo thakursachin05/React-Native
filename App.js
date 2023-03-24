5d19808441c8805ffc8cc7a3e00d139e688fc61e
SACHIN RANA <sachinrana@SACHINs-MacBook-Air.local>
2023-03-24 19:08:12 +0530

    Added the code

--- App.js
+++ App.js
@@ -1,59 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Linking, RefreshControl, ScrollViewBase, StyleSheet, Text, View } from 'react-native';
-import React, {useState, useSyncExternalStore} from 'react';
+import { Button, FlatList, Linking, RefreshControl, ScrollView, SectionList, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useSyncExternalStore } from 'react';
 
 export default function App() {
-    const [Items, setItems] = useState([
-      {key:1,item: 'Item 1'},
-      {key:2,item: 'Item 2'},
-      {key:3,item: 'Item 3'},
-      {key:4,item: 'Item 4'},
-      {key:5,item: 'Item 5'},
-      {key:6,item: 'Item 6'},
-      {key:7,item: 'Item 7'},
-      {key:8,item: 'Item 8'}
-    ]);
-    const [Refreshing , setRefreshing] = useState(false);
-    const onRefresh = () => {
-      setRefreshing(true);
-      setItems([...Items , {key:69, item:"Item 69"}]);
-      setRefreshing(false);
+  const [Items, setItems] = useState([
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+    { name: 'Item 3' },
+    { name: 'Item 4' },
+    { name: 'Item 5' },
+    { name: 'Item 6' },
+    { name: 'Item 7' },
+    { name: 'Item 8' }
+  ]);
+  const [Refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, { key: 69, item: "Item 69" }]);
+    setRefreshing(false);
+  }
+
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1','Item 1-2', 'Item 1-3']
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1','Item 2-2', 'Item 2-3']
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1','Item 3-3']
+    },
+    {
+      title: 'Title 4',
+      data: ['Item 4-1']
     }
+  ]
   return (
-    // <View style={styles.container}>
-      <ScrollView style={styles.container} refreshContro={
-        <RefreshControl refreshing = {Refreshing} onRefresh = {onRefresh} color={['#ff00ff']}/>
-      }>
-      {
-        Items.map((object) => {
-          return(
-            <View style={styles.item} key={object.key}>
-              <Text style={styles.text}>{object.item}</Text>
-            </View>
-          )
-        })
-      }
-      </ScrollView>
-    // </View>
+    // <FlatList
+    //   // horizontal
+    //   // inverted
+    //   //  numColumns={4}   
+    //   data={Items}
+    //   keyExtractor={(item, index) => index.toString()}
+    //   renderItem={({ item }) => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.text}>{item.name}</Text>
+    //     </View>
+    //   )} 
+      
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={Refreshing}
+    //         onRefresh={onRefresh}
+    //         colors={['#ff00ff']}
+    //       />
+    //     }
+    //   />
+    // <View>
+
+      <SectionList 
+        keyExtractor={(item, index) => index.toString()}
+        sections={DATA}
+        renderItem = {({item})=>(
+          // <View style={styles.item}>
+          <Text style={styles.text}>{item}</Text>
+          //  </View>
+          )}
+          renderSectionHeader = {({section})=>(
+            <View style={styles.item}>
+              <Text style={styles.text}>{section.title}</Text>
+          </View>
+        )}
+        
+        />
+          /* <Button>Click</Button>
+        </View> */
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     flexDirection: 'column',
     backgroundColor: '#f000ff',
     alignItems: 'stretch',   // align items always workd in cross axis to the flex direction provided 
     justifyContent: 'center',
   },
-  item : {
+  item: {
     margin: 30,
     backgroundColor: '#4ae1fa',
     justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
-    color: '#ffffff',
+    color: '#000',
     fontSize: 25,
     fontStyle: 'italic',
     margin: 10,
@@ -61,3 +105,7 @@ const styles = StyleSheet.create({
   },
 
 });
+
+
+
+// Practice the ideal questions for this and then conintue ahead
