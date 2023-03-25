df46bc1522296052ec63b3fa2e6a42730ba746aa
thakursachin05 <sachinrajput10101010@gmail.com>
2023-03-25 13:22:22 +0530

    Added the code

--- App.js
+++ App.js
@@ -1,40 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, FlatList, Linking, Pressable, RefreshControl, ScrollView, SectionList, StyleSheet, Text, TextInput, ToastAndroid, Touchable, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import { Alert, Button, Modal, FlatList, Linking, Pressable, RefreshControl, ScrollView, SectionList, StyleSheet, Text, TextInput, ToastAndroid, Touchable, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import React, { useState, useSyncExternalStore } from 'react';
 
 export default function App() {
   const [name, setName] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
   const onPressHandler = () => {
     if (name.length > 3) {
       setSubmitted(!submitted);
     }
     else {
-      // on ios we can have any number of button 
-      // on andriod we can only have 3 text
-      // Alert.alert('Warning', "The name must be longer than 3 characters", [
-      //   { text: 'Do not show again', onPress: () => console.warn('Do not show Pressed!') },
-      //   { text: 'Cancel', onPress: () => console.warn('Cancel Pressed!') },
-      //   { text: 'OK', onPress: () => console.warn('Ok Pressed!') },
-      // ], { cancelable: true, onDismiss: () => console.warn('Alert Dismissed!') })
-      // ToastAndroid.show('The name must be longer than 3 characters',
-      //   ToastAndroid.LONG
-      // )
-      // ToastAndroid.showWithGravity('The name must be longer than 3 characters',
-      //   ToastAndroid.LONG,
-      //   ToastAndroid.CENTER
-      // )
-      ToastAndroid.showWithGravityAndOffset('The name must be longer than 3 characters',
-      ToastAndroid.LONG,
-      ToastAndroid.TOP,
-      0, // from top
-      0 // from left
-    )
+      setShowWarning(true);
     }
   }
 
   return (
     <View style={styles.body}>
+      <Modal
+      visible={showWarning}
+      onRequestClose={() => {
+        setShowWarning(false)
+      }}
+      transparent
+      animationType='slide'
+      hardwareAccelerated
+      >
+        <View style={styles.centered_view}>
+          <View style={styles.warning_modal}>
+            <View style={styles.warning_title}>
+              <Text style={styles.text}>WARNING!</Text>
+            </View>
+            <View style={styles.warning_body}>
+              <Text style={styles.text}>The name must be longer than 3 characters</Text>
+            </View>
+            <Pressable
+              onPress={()=> setShowWarning(false)}
+              style={styles.warning_button}
+              android_ripple={{color: '#fff'}}
+            >
+              <Text style={styles.text}>OK</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <Text style={styles.text}>
         Please write your name :
       </Text>
@@ -44,49 +53,6 @@ export default function App() {
         placeholder='e.g. Jai Shree Ram'
         onChangeText={(value) => setName(value)}
       />
-
-      {/* <Button 
-      title={submitted ? 'Clear' : 'Submit'}
-      onPress={onPressHandler}
-      disabled={submitted}
-      color= '#00f'
-      /> */}
-      {/* 
-      <TouchableOpacity
-        onPress={onPressHandler}
-        style = {styles.Button}
-        activeOpacity={0.2}
-      >
-        <Text style={styles.text}>
-          {submitted ? 'Clear' : 'Submit'}
-        </Text>
-      </TouchableOpacity> */}
-
-      {/* The only difference between touchable highlight or opactiy is support color change while touching */}
-
-      {/* <TouchableHighlight
-        onPress={onPressHandler}
-        style = {styles.Button}
-        activeOpacity={0.2}
-        underlayColor='#dddddd'
-      >
-        <Text style={styles.text}>
-          {submitted ? 'Clear' : 'Submit'}
-        </Text>
-      </TouchableHighlight> */}
-
-      {/* <TouchableWithoutFeedback
-        onPress={onPressHandler}
-        activeOpacity={0.2}
-        underlayColor='#dddddd'
-      > 
-        <View  style = {styles.Button}>
-          <Text style={styles.text}>
-            {submitted ? 'Clear' : 'Submit'}
-          </Text>
-        </View>
-      </TouchableWithoutFeedback> */}
-
 
       <Pressable
         onPress={onPressHandler}
@@ -127,8 +93,8 @@ const styles = StyleSheet.create({
   text: {
     color: '#000',
     fontSize: 20,
-    fontStyle: 'italic',
     margin: 10,
+    textAlign: 'center'
   },
   Button: {
     width: 150,
@@ -144,6 +110,39 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     marginBottom: 10,
+  },
+  warning_modal: {
+    width: 300,
+    height: 300,
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 20,
+  },
+  centered_view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000099'
+  },
+  warning_title: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff0',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warning_body: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  warning_button: {
+    backgroundColor: '#00ffff',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    margin: 0,
   }
 
 });
